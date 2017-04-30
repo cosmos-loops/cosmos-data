91dd939772f6f0a762a51804467b1d1abbb248b1
alexinea <forerunner@outlook.com>
2017-04-30 20:26:06 +0800

    Add compression extensions for asp.net

---
+++ src/Cosmos.AspNet.Extensions.Compression/CompressionAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System.IO.Compression;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cosmos.AspNet.Extensions
+{
+    /// <summary>
+    /// 压缩
+    /// </summary>
+    public class CompressionAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// action executing...
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var preferredEncoding = GetPreferredEncoding(filterContext.HttpContext.Request);
+
+            var response = filterContext.HttpContext.Response;
+            response.AppendHeader("Content-Encoding", preferredEncoding.ToString());
+
+            if (preferredEncoding == CompressionScheme.Gzip)
+            {
+                response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+            }
+            else if (preferredEncoding == CompressionScheme.Deflate)
+            {
+                response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
+            }
+        }
+
+        private CompressionScheme GetPreferredEncoding(HttpRequestBase request)
+        {
+            var acceptableEncoding = request.Headers["Accept-Encoding"].ToLower();
+            if (string.IsNullOrEmpty(acceptableEncoding))
+            {
+                return CompressionScheme.Identity;
+            }
+
+            if (acceptableEncoding.Contains("gzip"))
+            {
+                return CompressionScheme.Gzip;
+            }
+
+            if (acceptableEncoding.Contains("deflate"))
+            {
+                return CompressionScheme.Deflate;
+            }
+
+            return CompressionScheme.Identity;
+        }
+    }
+}
---
+++ src/Cosmos.AspNet.Extensions.Compression/CompressionScheme.cs
@@ -0,0 +1,9 @@
+﻿namespace Cosmos.AspNet.Extensions
+{
+    enum CompressionScheme
+    {
+        Gzip = 0,
+        Deflate = 1,
+        Identity = 2
+    }
+}
