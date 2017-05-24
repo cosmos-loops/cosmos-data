c51ca1a1cd3f2cc7ff1d5b819a226291903c9d89
alexinea <forerunner@outlook.com>
2017-05-24 16:55:58 +0800

    添加 *Constants 的注释

--- src/Cosmos.AspNet.Extensions.Alipay/Internal/AlipayConstants.cs
+++ src/Cosmos.AspNet.Extensions.Alipay/Internal/AlipayConstants.cs
@@ -5,7 +5,14 @@
     /// </summary>
     public static class AlipayConstants
     {
+        /// <summary>
+        /// Alipay Browser Identity
+        /// </summary>
         public static readonly string AlipayBrowserIdentity = "Alipay";
+
+        /// <summary>
+        /// Alipay Browser Only
+        /// </summary>
         public static readonly string AlipayBrowserOnly = "Alipay Browser Only";
     }
 }
--- src/Cosmos.AspNet.Extensions.WeChat/Internal/WeChatConstants.cs
+++ src/Cosmos.AspNet.Extensions.WeChat/Internal/WeChatConstants.cs
@@ -5,7 +5,14 @@
     /// </summary>
     public static class WeChatConstants
     {
+        /// <summary>
+        /// WeChat Broswer Identity
+        /// </summary>
         public static readonly string WeChatBrowserIdentity = "MicroMessenger";
+
+        /// <summary>
+        /// WeChat Browser Only
+        /// </summary>
         public static readonly string WeChatBrowserOnly = "WeChat Browser Only";
     }
 }
