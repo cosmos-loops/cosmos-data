5280f2c52b932c2678740ebce49a309d32291f40
alexinea <forerunner@outlook.com>
2017-05-29 23:14:57 +0800

    规范 Filter 扩展方法的命名方式

---
+++ src/Cosmos.AspNet.Extensions.ResponseCompression/ResponseCompressionFilterExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Cosmos.AspNet.Extensions
+{
+    /// <summary>
+    /// Response compression filter extensions
+    /// </summary>
+    public static class ResponseCompressionFilterExtensions
+    {
+        /// <summary>
+        /// 全局使用 Compression filter
+        /// </summary>
+        /// <param name="filters"></param>
+        /// <returns></returns>
+        public static GlobalFilterCollection AddResponseCompressionFilter(this GlobalFilterCollection filters)
+        {
+            if (filters == null)
+            {
+                throw new ArgumentNullException(nameof(filters));
+            }
+
+            filters.Add(new CompressionAttribute());
+
+            return filters;
+        }
+    }
+}
