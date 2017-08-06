1760a689edfb4430512f3a12c1426902b904e760
alexinea <forerunner@outlook.com>
2017-08-06 19:55:28 +0800

    Change assembly name

--- src/Cosmos.AspNet.Extensions.HttpActionResults/Properties/AssemblyInfo.cs
+++ src/Cosmos.AspNet.Extensions.HttpActionResults/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Cosmos.AspNet.Extensions.HttpActionResult")]
+[assembly: AssemblyTitle("Cosmos.AspNet.Extensions.HttpActionResults")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cosmos.AspNet.Extensions.HttpActionResult")]
+[assembly: AssemblyProduct("Cosmos.AspNet.Extensions.HttpActionResults")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
